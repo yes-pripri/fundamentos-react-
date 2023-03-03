81bd92ab57b98dd97e59ee202d4cb5dc6c7e3642
JuniorSelister <junior.selister@gmail.com>
2023-03-03 14:11:10 -0300

    Fix in files

--- fundamentos-react/src/components/Comment.jsx
+++ fundamentos-react/src/components/Comment.jsx
@@ -2,32 +2,32 @@ import { Trash, ThumbsUp } from 'phosphor-react';
 import styles from './Comment.module.css';
 
 export function Comment() {
-    return (
-        <div className={styles.comment}>
-            <img src="https://avatars.githubusercontent.com/u/87504113?v=4"/>
-            <div className={styles.commentBox}>
-            <div className={styles.commentComment}>
-                <header>
-                    <div className={styles.authorAndTime}>
-                        <strong>Kathelen Priscila</strong>
-                        <time title="27 de Fevereiro às 4:10"    dateTime='2023-02-27 06:16'>Cerca de 1hr atrás</time>
-                    </div>
-                    <button title="Deletar comentário">
-                        <Trash size={20} />
-                    </button>
+  return (
+    <div className={styles.comment}>
+      <img src="https://avatars.githubusercontent.com/u/87504113?v=4"/>
 
-                    <p>Muito bom Devon, parabéns!! 👏👏</p>
-
-                </header>
-
-                <footer>
-                    <button>
-                        <ThumbsUp />
-                        Aplaudir <span>20</span>
-                    </button>
-                </footer>
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Kathelen Priscila</strong>
+              <time title="27 de Fevereiro às 4:10" dateTime='2023-02-27 06:16'>Cerca de 1hr atrás</time>
             </div>
-            </div>
+            <button title="Deletar comentário">
+                <Trash size={24} />
+            </button>
+
+          </header>
+            <p>Muito bom Devon, parabéns!! 👏👏</p>
         </div>
-    )
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
 }
--- fundamentos-react/src/components/Sidebar.jsx
+++ fundamentos-react/src/components/Sidebar.jsx
@@ -2,29 +2,28 @@ import { PencilLine } from 'phosphor-react';
 import styles from './Sidebar.module.css';
 
 export function Sidebar() {
-    return (
-       <aside className={styles.sidebar}>
-            <img 
-            className={styles.cover} 
-            src='https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'
-            />
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src='https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50'
+      />
 
-            <div className={styles.profile}>
+      <div className={styles.profile}>
+        <img 
+          className={styles.avatar} src="https://avatars.githubusercontent.com/u/87504113?v=4" 
+        />
+    
+        <strong>Kathelen Priscila</strong>
+        <span>Web developer</span>
+      </div>
 
-                <img 
-                className={styles.avatar} src="https://avatars.githubusercontent.com/u/87504113?v=4" 
-                />
-            
-                <strong>Kathelen Priscila</strong>
-                <span>Web developer</span>
-            </div>
-
-            <footer>
-                <a href="#">
-                    <PencilLine size={20} />
-                    Editar seu perfil 
-                </a>
-            </footer>
-        </aside>
-    );
+      <footer>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil 
+          </a>
+      </footer>
+    </aside>
+  );
 }
